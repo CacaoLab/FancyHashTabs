e180af8b5eb58503ec06823afaa468d404a85a27
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-10-02 07:29:52 -0500

    Update the description message

--- FancyTabs.php
+++ FancyTabs.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Fancy Tabs with multiple effects
  * Plugin URI: https://github.com/CacaoLab/FancyTabs
- * Description: Plugin to insert multiple markers in a Location. Use the Hash Tabs Library as base.
+ * Description: Allows to insert content in tabs. Use the Hash Tabs Library as base.
  * Version: 0.1
  * Author: Crisoforo Gaspar
  * Author URI: http://www.crisoforo.com
