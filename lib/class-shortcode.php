ecde698eb301d2d35eebd01fe07ba52ccc4924f5
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-10-04 13:53:04 -0500

    Add more capavility to handle inner shortcodes

--- lib/class-shortcode.php
+++ lib/class-shortcode.php
@@ -1,11 +1,36 @@
 <?php namespace com\github\cacaolab\lib;
 
 class Shortcode extends Wordpress{
-    public function create( $atts, $content = null){
-        return 'Hash Tabs';
+    public function hashtabs_shortcode( $atts, $content = null){
+        return
+            '<div id="Fancy-Hash-Tabs">' .
+            \do_shortcode( $content ) .
+            '</div>';
+    }
+
+    public function tab_title_shortcode( $atts, $content = null ){
+        extract( 
+            shortcode_atts( array(
+                'title' => 'Tab'
+            ), $atts )
+        );
+
+        return 
+            '<li>' .
+            $title .
+            '</li>';
+    }
+
+    public function tab_shortcode( $atts, $content = null ){
+        return 
+            '<div>' .
+            $content .
+            '</div>';
     }
 
     public function register(){
-        add_shortcode( 'hashtabs', $this->call_method('create') );
+        add_shortcode( 'hashtabs', $this->call_method('hashtabs_shortcode') );
+        add_shortcode( 'tabTitle', $this->call_method('tab_title_shortcode') );
+        add_shortcode( 'tab', $this->call_method('tab_shortcode') );
     }
 }
