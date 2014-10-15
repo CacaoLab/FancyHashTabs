e39898f435e293d91a355cf6c4b63fda9d3e8cbe
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-10-14 21:14:14 -0500

    Change some methods name and implementation code

--- lib/class-assets.php
+++ lib/class-assets.php
@@ -2,28 +2,35 @@
 
 class Assets extends Wordpress {
 
-    private $path = '';
-
+    private $plugin_path = '';
     private $assets_path = '';
 
     public function __construct( $path ) {
-        $this->path = $path;
-        $this->assets_path = $this->path . '/assets';
+        $this->plugin_path = $path;
+        $this->assets_path = $this->plugin_path . '/assets';
         $this->register();
     }
 
     private function register(){
-        wp_register_script('jquery-ba-hash-change', $this->get_path_from_JS_asset( 'jquery.ba-hashchange.min.js' ), array('jquery'), null, false );
-        wp_register_script('jquery-easy-tabs', $this->get_path_from_JS_asset( 'jquery.easytabs.min.js' ), array('jquery'), null, false );
-        wp_register_style('easy-tabs-styles', $this->assets_path . '/css/example.css', array(), null, null);
+        $this->register_css();
+        $this->register_js();
     }
 
-    private function get_path_from_JS_asset( $file_name ){
-        return ( ( $this->get_JS_directory() ) . '/' . $file_name ); 
+    private function register_css(){
+        wp_register_style('easy-tabs-styles', $this->get_asset_path( 'css',  'example.css' ), array(), null, null);
     }
 
-    private function get_JS_directory(){
-        return ($this->assets_path . '/js');
+    private function register_js(){
+        wp_register_script('jquery-ba-hash-change', $this->get_asset_path( 'js', 'jquery.ba-hashchange.min.js' ), array('jquery'), null, false );
+        wp_register_script('jquery-easy-tabs', $this->get_asset_path( 'js', 'jquery.easytabs.min.js' ), array('jquery'), null, false );
+    }
+
+    private function get_asset_path( $directory_name = '', $file_name = '' ){
+        return $this->get_path_from_asset($directory_name) . $file_name;
+    }
+
+    private function get_path_from_asset( $directory_name ){
+        return ( $this->assets_path . '/' . $directory_name . '/' );
     }
 
     public function load(){
